1a03c65793950cae6e6d1774d0ce4dbc4e58c708
Venkatesh Anagandula <vanagandula@nisum.com>
2020-01-14 11:40:34 +0530

    Inital commit

---
+++ src/test/java/com/nisum/SpringbootConfigServer/SpringbootConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.SpringbootConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
